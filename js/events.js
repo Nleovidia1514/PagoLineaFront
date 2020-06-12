3cddfaceafcffb153367364008c2ead7ca25eece
Leonardo Baron <leonardo.1234baron@gmail.com>
2020-06-12 18:50:57 -0400

    cambiados colores

--- js/events.js
+++ js/events.js
@@ -13,16 +13,33 @@ const debounce = fn => {
 const storeScroll = () => {
   if (window.scrollY === 0) {
     document.querySelector('header').classList.add('opaque');
+    
     // document
     //   .querySelector('.logo-web')
     //   .setAttribute('src', 'assets/logos/web-logo-white.png');
   } else {
     document.querySelector('header').classList.remove('opaque');
+    if (document.querySelector('.menu').classList.contains('open')) {
+
+      document.querySelector('.menu-toggler').style.color = 'black';
+    } 
     // document
     //   .querySelector('.logo-web')
     //   .setAttribute('src', 'assets/logos/web-logo.png');
   }
 };
+
+setInterval(() => {
+  if(window.scrollY === 0) {
+    if (document.querySelector('.menu').classList.contains('open')) {
+      document.querySelector('.menu-toggler').style.color = 'white';
+    } else {
+      document.querySelector('.menu-toggler').style.color = 'black';
+    }
+  }else {
+    document.querySelector('.menu-toggler').style.color = 'black';
+  }
+}, 200)
 
 document.addEventListener('scroll', debounce(storeScroll), { passive: true });
 
